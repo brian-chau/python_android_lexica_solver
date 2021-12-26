a849e89846fbe6aabdc0832b92db10ab6d44e2fb
Brian Chau <brian.chau@outlook.com>
2021-12-26 12:28:57 -0800

    first commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+# Python implementation of a solver for Lexica Android game
+
+Lexica is an open-source Android game available on F-Droid. Here is the [source code](https://github.com/lexica/lexica) on GitHub.
+
+The goal of the game is to find as many words as possible in the grid, where the word can go up, down, left, right, and/or diagonal in the grid.
+
+This source code is borrowed from a Leetcode solution [here](https://leetcode.com/problems/word-search/discuss/1648500/Python-backtrack-solution), where I added the code to look in the diagonal directions as well.
---
+++ solver.py
@@ -0,0 +1,52 @@
+class Solution:
+    def backtrack_exist(self, board, x, y, word, k):
+        if k == len(word):
+            return True
+        if not (0 <= x < len(board) and 0 <= y < len(board[0])):
+            return False
+
+        if board[x][y] != word[k]:
+            return False
+
+        board[x][y] = ord(board[x][y]) ^ 256
+        exist = any(
+            (
+                self.backtrack_exist(board, x,     y + 1, word, k + 1),
+                self.backtrack_exist(board, x,     y - 1, word, k + 1),
+                self.backtrack_exist(board, x + 1, y,     word, k + 1),
+                self.backtrack_exist(board, x + 1, y + 1, word, k + 1),
+                self.backtrack_exist(board, x - 1, y + 1, word, k + 1),
+                self.backtrack_exist(board, x + 1, y - 1, word, k + 1),
+                self.backtrack_exist(board, x - 1, y - 1, word, k + 1)
+            )
+        )
+        board[x][y] = chr(board[x][y] ^ 256)
+        return exist
+
+    def exist(self, board, word) -> bool:
+        m = len(board)
+        n = len(board[0])
+
+        for i in range(m):
+            for j in range(n):
+                exist = self.backtrack_exist(board, i, j, word, 0)
+                if not exist:
+                    continue
+                return exist
+
+        return False
+
+
+if __name__ == '__main__':
+    obj = Solution()
+
+    with open('board.txt') as f:
+        board = [list(line.lower().strip()) for line in f.readlines()]
+
+    with open('words.txt') as f:
+        words = [line.lower().strip() for line in f.readlines()]
+
+    with open('output.txt', 'w+') as f:
+        for word in words:
+            if obj.exist(board, word) and len(word) >= 3:
+                f.write(word + '\n')
